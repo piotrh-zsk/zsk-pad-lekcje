3b8eba6f108803abaa4033782b9dea841fe1eec1
Piotr Hamrol <piotr.hamrol@plk-sa.pl>
2023-11-27 19:16:10 +0100

    Projekt 01 - Etap 07 - Wytyczne

---
+++ Projekt_01/Etap_07/CompressForm.cs
@@ -0,0 +1,39 @@
+﻿using Projekt_01_Etap_06_Rozwiazanie.Presenters;
+
+namespace Projekt_01_Etap_06_Rozwiazanie
+{
+    public partial class CompressForm : Form
+    {
+        private CompressFormPresenter? compressFormPresenter;
+
+        public CompressForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CompressForm_Load(object sender, EventArgs e)
+        {
+             compressFormPresenter = new CompressFormPresenter(
+                tb_Kompresowanie_CzasWyznaczeniaKodow,
+                tb_Kompresowanie_CzasKompresji,
+                dgv_Kompresowanie_Kody);
+        }
+
+
+        private void bt_SpakujPlik_Click(object sender, EventArgs e)
+        {
+            string currentPath = Environment.CurrentDirectory;
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = currentPath;
+                ofd.Filter = "Pliki tekstowe|*.txt";
+                var result = ofd.ShowDialog(this);
+                if (result == DialogResult.OK)
+                {
+                    var fileText = File.ReadAllText(ofd.FileName);
+                    compressFormPresenter?.PerformTextCompression(ofd.SafeFileName, fileText);
+                }
+            }
+        }
+    }
+}
