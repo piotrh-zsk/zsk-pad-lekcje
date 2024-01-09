53d4c8ccb24c3d0115058ea2cb8d2a3cc94504f6
Piotr Hamrol <piotr.hamrol@plk-sa.pl>
2024-01-09 08:39:07 +0100

    Etap 1 - Rozwiązanie

---
+++ Semestr 2/Projekt/Etap_01/Rozwiazanie/WpfApp1/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    public class Clock : INotifyPropertyChanged
+    {
+        public string CurrentTime => DateTime.Now.ToLongTimeString();
+
+        //public string CurrentTime
+        //{
+        //    get { return DateTime.Now.ToLongTimeString(); }
+        //}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private DispatcherTimer _timer;
+        public Clock()
+        {
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _timer.Tick += (sender, o) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentTime)));
+            _timer.Start();
+        }
+    }
+}
